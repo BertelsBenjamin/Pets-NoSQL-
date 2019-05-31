3508f186cb53942a3a938fb947f938bfcb89633e
BertelsBenjamin <bertels123@gmail.com>
2019-05-31 20:51:34 +0200

    select table 2.0

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 
   //VARIABLES & FUNCTIONS
   let selectedTable = "pet";
+  let tables;
 
 
   function clearTable() {
@@ -19,9 +20,10 @@
         console.log(tables);
         for (let i = 0; i < tables.length; i++) {
           $("#select-table").append(
-            "<option selected='true' class='form-option' value='" + tables[i].Tables_in_petslog + "' " + ">" + tables[i].Tables_in_petslog + "</option>"
+            "<option selected='' class='form-option' value='" + tables[i].Tables_in_petslog + "' " + ">" + tables[i].Tables_in_petslog + "</option>"
           )
         }
+        $('option:first-child').prop('selected', 'true');
       })
   }
 
@@ -43,15 +45,17 @@
 
         if (selectedTable = 'event') {
           clearTable();
-          data.map(e => {
+          data.map(x => {
+            console.log(x);
             $('#table-body').append(
-              '<tr class="table-row"><td>' + e.id + '</td>' + '<td>' + e.date + '</td>' + '<td>' + e.reftopet + '</td>' + '<td>' + e.type + '</td>' + '<td>' + e.remark + '</td></tr>'
+              '<tr class="table-row"><td>' + x.id + '</td><td>' + x.reftopet + '</td><td>' + x.date + '</td><td>' + x.type + '</td><td>' + x.remark + '</td></tr>'
             );
           })
         }
-
-        if (selectedTable = 'pet') {
+        
+        if(selectedTable = 'pet') {
           clearTable();
+          console.log(selectedTable);
           data.map(e => {
             if (e.death === null) {
               e.death = "Still alive";
@@ -81,21 +85,35 @@
   $(document).ready(function () {
     console.log(selectedTable);
     getDatabaseTables();
+
+    //LOAD TABLE BY DEFAULT
+    clearTable();
+    loadTable(selectedTable);
+
+    //SELECT --> CHANGE TABLE
     $('select').change(function () {
       $('select option:selected').each(function () {
-        selectedTable = $(this).text();
+        getDatabaseTables();
+        console.log(tables);
+        for(i=0; i<tables.Tables_in_petslog.length; i++){
+        if($(this).text() = tables.Tables_in_petslog[i]){
+          selectedTable = tables.Tables_in_petslog[i].text;
+        };
+      }
         console.log(selectedTable);
       })
       clearTable();
       loadTable(selectedTable);
     });
+
+    //INSERT --> INSERT ROW AND REFRESH TABLE
     $('#btn-insert-row').click(function () {
       addRow();
       clearTable();
       loadTable(selectedTable);
     })
-    clearTable();
-    loadTable(selectedTable);
+
+    
   })
 
 })(jQuery);
