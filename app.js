2d0fd8166602ac526bba667714132c3a15fc2b60
BertelsBenjamin <bertels123@gmail.com>
2019-06-19 21:33:25 +0200

    alternative routing

--- app.js
+++ app.js
@@ -1,58 +1,33 @@
 //VARIABLES, FUNCTIONS & REQUIREMENTS
 const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const urlencode = bodyParser.urlencoded({ extended: true });
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'petslog',
-    port: '3307'
-});
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-const $ = jQuery = require('jquery')(window);
 
-function connectWithDatabase() {
-    connection.connect((err) => {
-        if (err) {
-            console.log(err);
-            console.log('Error connecting to Db');
-            return;
-        }
-        else {
-            console.log('Connected')
-        };
-    });
-}
+mongoose.connect('mongodb://localhost:27017/petslog', { useNewUrlParser: true });
+mongoose.connection
+    .on('connected', () => { console.log('Connected') })
+    .on('error', (err) => { console.log('error', err.message) })
 
-function queryToDatabase(query, req, res) {
-    connection.query(query, (err, rows, fields) => {
-        if (err) {
-            console.log(err);
-            throw err;
-            debugger;
-        }
-        else {
-            console.log('Data received');
-            res.send(JSON.stringify(rows));
-            console.log(fields);
-        }
-    });
-}
+const petSchema = new Schema({
+    name: String,
+    owner: String,
+    species: String,
+    sex: String,
+    birth: Date
+})
 
-function disconnectFromDatabase() {
-    connection.end((err) => {
-        console.log('Disconnected');
-    });
-}
+const eventSchema = new Schema({
+    date: Date,
+    type: String,
+    remark: String,
+})
 
-connectWithDatabase();
+const petslog = [];
+petslog['pet'] = mongoose.model('Pet', petSchema);
+petslog['event'] = mongoose.model('Event', eventSchema);
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -62,43 +37,51 @@ app.use(function (req, res, next) {
     //Verwijder caching om de laatste data te ontvangen
     res.setHeader('Cache-Control', 'no-cache');
     next();
-    console.log(req);
+    //console.log(req);
 });
 
-/*Post something to port 3000 */
-app.get('/', function (req, res) {
-    res.send('You have sent this message via res.send in an app.get request in app.js to port 3000');
-})
-
-app.post('/getTables', urlencode, function (req, res) {
-    connectWithDatabase();
-    queryToDatabase('SHOW TABLES;', req, res);
-})
-
-/*Post received data (based on query) via main.js to index.html (viewable on VS Code Live Server (port 550x)) */
-app.post('/load/:selectedTable', urlencode, function (req, res) {
-    connectWithDatabase();
-    console.log(req.params);
-    queryToDatabase('SELECT * FROM ' + req.params.selectedTable + ';', req, res);
-})
-
-app.post('/insertRow', (req, res) => {
-    let sql = "INSERT INTO event (reftopet, date, type, remark) VALUES ?";
-    let values = [
-        [8, '2019-05-21', 'litter', 'broken rib']
-    ]
-    connection.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log('Number of records inserted: ' + result.affectedRows);
+//VIEWABLE IN LOCALHOST:3000
+app.get('/', (req, res) => {
+    petslog['event'].find({}, (err, rows) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(rows);
+            res.send(rows);
+        }
     })
-    res.send("Data inserted.")
 })
 
-app.delete('/', (req, res) => {
-    res.send("Delete");
+/*GET ALL PETS */
+app.post('/pet', urlencode, (req, res) => {
+    mongoose.model('pet', petSchema).find({}, (err, rows) => {
+        if (err) {
+            console.log(err)
+            res.send(err);
+        }
+        else {
+            console.log(rows);
+            res.json(rows);
+        }
+    })
+})
+
+/*GET ALL EVENTS */
+app.post('/event', urlencode, (req, res) => {
+    mongoose.model('event', eventSchema).find({}, (err, rows) => {
+        if (err) {
+            console.log(err)
+            res.send(err);
+        }
+        else {
+            console.log(rows);
+            res.json(rows);
+        }
+
+    })
+
 })
 
 /*Port watcher (listener) */
 app.listen('3000', () => console.log('app listening on port 3000'));
-
-//disconnectFromDatabase();
--- main.js
+++ main.js
@@ -1,119 +1,41 @@
 //changes to index.html, style.css and main.js are displayed on localhost:5000
 (function ($) {
 
-  //VARIABLES & FUNCTIONS
-  let selectedTable = "pet";
-  let tables;
-
-
-  function clearTable() {
-    $('#table-body tr').remove();
-  }
-
-  function getDatabaseTables() {
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:3000/getTables'
-    })
-      .done(function (tables) {
-        tables = JSON.parse(tables);
-        console.log(tables);
-        for (let i = 0; i < tables.length; i++) {
-          $("#select-table").append(
-            "<option selected='' class='form-option' value='" + tables[i].Tables_in_petslog + "' " + ">" + tables[i].Tables_in_petslog + "</option>"
-          )
-        }
-        $('option:first-child').prop('selected', 'true');
-      })
-  }
-
-  function loadTable(selectedTable) {
-    $.ajax({
-      method: "POST",
-      url: "http://localhost:3000/load/" + selectedTable,
-    })
-      .done(function (data) {
-        data = JSON.parse(data);
-        console.log(data);
-        $('#table-head tr td').remove();
-        for (i = 0, j = 0; i < Object.keys(data[j]).length; i++ , j++) {
-          let keysArr = Object.keys(data[i]);
-          $('#table-header').append(
-            '<td>' + keysArr[j] + '</td>'
-          )
-        }
-
-        if (selectedTable = 'event') {
-          clearTable();
-          data.map(x => {
-            console.log(x);
-            $('#table-body').append(
-              '<tr class="table-row"><td>' + x.id + '</td><td>' + x.reftopet + '</td><td>' + x.date + '</td><td>' + x.type + '</td><td>' + x.remark + '</td></tr>'
-            );
-          })
-        }
-        
-        if(selectedTable = 'pet') {
-          clearTable();
-          console.log(selectedTable);
-          data.map(e => {
-            if (e.death === null) {
-              e.death = "Still alive";
-            }
-            $('#table-body').append(
-              '<tr class="table-row"><td>' + e.id + '</td><td>' + e.name + '</td><td>' + e.owner + '</td><td>' + e.species + '</td><td>' + e.sex + '</td><td>' + e.birth + '</td><td>' + e.death + '</td></tr>'
-            );
-          })
-        }
-
-      });
-  }
-
-  function addRow() {
-    let inputArr = $("input");
-    console.log(inputArr);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:3000/insertRow",
-    })
-      .done(function (msg) {
-        console.log(msg);
-        console.log(inputArr);
-      });
-  }
-
+  /*DOCUMENT READY */
   $(document).ready(function () {
-    console.log(selectedTable);
-    getDatabaseTables();
-
-    //LOAD TABLE BY DEFAULT
-    clearTable();
-    loadTable(selectedTable);
-
-    //SELECT --> CHANGE TABLE
-    $('select').change(function () {
-      $('select option:selected').each(function () {
-        getDatabaseTables();
-        console.log(tables);
-        for(i=0; i<tables.Tables_in_petslog.length; i++){
-        if($(this).text() = tables.Tables_in_petslog[i]){
-          selectedTable = tables.Tables_in_petslog[i].text;
-        };
-      }
-        console.log(selectedTable);
-      })
-      clearTable();
-      loadTable(selectedTable);
-    });
-
-    //INSERT --> INSERT ROW AND REFRESH TABLE
-    $('#btn-insert-row').click(function () {
-      addRow();
-      clearTable();
-      loadTable(selectedTable);
+    $('#btn-pets').click(function () {
+      $.ajax({
+          method: 'POST',
+          url: "http://localhost:3000/" + 'pet',
+          dataType: "json"
+        })
+        .done({
+          function (data) {
+            console.log('Call is done.')
+            data = JSON.parse(data);
+            console.log('Data: ', data);
+          }
+        })
+        .fail(function (err) {
+          console.log(err);
+        })
     })
 
-    
+    $('#btn-events').click(function () {
+      $.ajax({
+          method: 'POST',
+          url: "http://localhost:3000/" + 'event'
+        })
+        .done({
+          function (data) {
+            //data = JSON.parse(data);
+            console.log('test');
+            console.log('data', data);
+          }
+        })
+        .fail(function (err) {
+          console.log(err);
+        })
+    })
   })
-
 })(jQuery);
